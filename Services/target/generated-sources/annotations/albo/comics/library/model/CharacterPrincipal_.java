42e9dbba667fffd0fd89cc712efa815ca77c21af
jacito <jazminvel74@gmail.com>
2021-07-21 20:53:31 -0500

    Actualización de docker

--- Services/target/generated-sources/annotations/albo/comics/library/model/CharacterPrincipal_.java
+++ Services/target/generated-sources/annotations/albo/comics/library/model/CharacterPrincipal_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-07-21T20:44:12")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-07-21T20:53:18")
 @StaticMetamodel(CharacterPrincipal.class)
 public class CharacterPrincipal_ { 
 
--- Services/target/generated-sources/annotations/albo/comics/library/model/Creator_.java
+++ Services/target/generated-sources/annotations/albo/comics/library/model/Creator_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-07-21T20:44:12")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-07-21T20:53:18")
 @StaticMetamodel(Creator.class)
 public class Creator_ { 
 
